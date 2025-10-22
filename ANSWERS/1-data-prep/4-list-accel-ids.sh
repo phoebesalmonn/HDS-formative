4d6ef280a7654e9a0d95a703e7f1a1316c95a88c
phoebesalmonn <phoebe.g.salmon@gmail.com>
2025-10-22 17:28:31 +0100

    stage deleted files (just figured out how, so not in branch soz

--- ANSWERS/1-data-prep/4-list-accel-ids.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-## get all the IDs from the accelerometer files and store as a list in a file
-ls ../../data/derived/accel | grep -Eo '[0-9]+' > ../../data/derived/accel/pids-with-accel.txt
